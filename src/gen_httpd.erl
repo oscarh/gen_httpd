1566c7e593a7c3263a571611c60ecbfad8a313df
Oscar Hellström <none@none>
2008-07-01 00:51:26 +0100

    * Sending through gen_tcpd now (no more SockMod in gen_httpd)   * Realised the gen_httpd process is not supervised, need to make the acceptor in to     a special process?

--- src/gen_httpd.erl
+++ src/gen_httpd.erl
@@ -8,7 +8,6 @@
 
 -record(state, {
 	socket,
-	sock_mod,
 	timeout,
 	callback,
 	callbackstate,
@@ -17,17 +16,15 @@
 
 start_link(Callback, CallbackArgs, Port, Timeout) ->
 	IntState = #state{
-		sock_mod = gen_tcp,
 		timeout = Timeout,
 		callback = Callback,
 		callbackstate = CallbackArgs,
 		buf = []
 	},
-	gen_tcpd:start_link(?MODULE, IntState, Port, [{active, false}]).
+	gen_tcpd:start_link(?MODULE, IntState, tcp, Port, [{active, false}]).
 
 start_link(Callback, CallbackArgs, Port, Timeout, SSL) ->
 	IntState = #state{
-		sock_mod = ssl,
 		timeout = Timeout,
 		callback = Callback,
 		callbackstate = CallbackArgs,
@@ -39,7 +36,7 @@ recv(#state{buf = Buf} = State, _T, Size) when length(Buf) >= Size ->
 	{Data, NewBuf} = lists:split(Buf, Size),
 	{Data, State#state{buf = NewBuf}};
 recv(#state{socket = Socket, buf = Buf} = State, T, Size) ->
-	case (State#state.sock_mod):recv(Socket, Size - length(Buf), T) of
+	case gen_tcpd:recv(Socket, Size - length(Buf), T) of
 		{ok, Packet} when length(Buf) + length(Packet) == Size ->
 			{Buf ++ Packet, State#state{buf = []}};
 		{ok, Packet} ->
@@ -58,6 +55,11 @@ init(#state{callback = Callback, callbackstate = CallbackArgs} = State) ->
 			exit({bad_return, Other})
 	end.
 
+% TODO supervise this prcocess since the TCP parent no longer cares.
+% XXX This should MOST probably be done by the gen_tcpd supervisor though,
+% since that is the one I used to start link :/ So, gen_tcpd supervisor
+% should care about / supervise it's acceptors, which will become this
+% process later. How does that work with receiving from sockets etc. 
 handle_connection(Socket, State) ->
 	NewState = State#state{socket = Socket},
 	{Req, Headers, Body} = receive_loop(NewState, []),
@@ -68,19 +70,19 @@ handle_request(State, {Method, Path, Vsn}, Headers) ->
 		{C, NS} ->
 			{C == close, NS};
 		{C, Reply, NS} ->
-			ok = (NS#state.sock_mod):send(NS#state.socket, Reply),
+			ok = gen_tcpd:send(NS#state.socket, Reply),
 			{C == close, NS}
 	end,
 	case {Vsn, lists:keysearch("Connection", 1, Headers), Close} of
 		{"HTTP/1.1", {value, {_, KeepAlive}}, true} when KeepAlive /= "close" ->
 			handle_connection(NewState#state.socket, State);
 		_ ->
-			(NewState#state.sock_mod):close(NewState#state.socket),
+			gen_tcpd:close(NewState#state.socket),
 			exit(normal)
 	end.
 
-receive_loop(#state{socket = Socket, sock_mod = SockMod, timeout = Timeout} = S, Acc) ->
-	case SockMod:recv(Socket, 0, Timeout) of
+receive_loop(#state{socket = Socket} = S, Acc) ->
+	case gen_tcpd:recv(Socket, 0, S#state.timeout) of
 		{ok, Packet} ->
 			case catch gen_httpd_parser:parse(Acc ++ Packet) of
 				{'EXIT', incomplete} ->
@@ -89,30 +91,36 @@ receive_loop(#state{socket = Socket, sock_mod = SockMod, timeout = Timeout} = S,
 					Result
 			end;
 		{error, timeout} ->
-			SockMod:close(Socket),
+			gen_tcpd:close(Socket),
 			exit(normal)
 	end.
 
 call_callback(State, "GET", Path, VSN, Headers) ->
-	(State#state.callback):handle_get(State, {get, Path, VSN}, Headers,
-		State#state.callbackstate);
+	Callback = State#state.callback,
+	CBState = State#state.callbackstate,
+	{State, Callback:handle_get(Path, VSN, Headers, CBState)};
 call_callback(State, "PUT", Path, VSN, Headers) ->
 	{Body, NewState} = handle_upload(State, Headers),
-	(State#state.callback):handle_put(NewState, {put, Path, VSN}, Headers,
-		Body, State#state.callbackstate);
+	Callback = State#state.callback,
+	CBState = State#state.callbackstate,
+	{NewState, Callback:handle_put(Path, VSN, Headers, Body, CBState)};
 call_callback(State, "HEAD", Path, VSN, Headers) ->
-	(State#state.callback):handle_head(State, {head, Path, VSN}, Headers,
-		State#state.callbackstate);
+	Callback = State#state.callback,
+	CBState = State#state.callbackstate,
+	{State, Callback:handle_head(Path, VSN, Headers, CBState)};
 call_callback(State, "POST", Path, VSN, Headers) ->
+	Callback = State#state.callback,
+	CBState = State#state.callbackstate,
 	{Body, NewState} = handle_upload(State, Headers),
-	(State#state.callback):handle_post(NewState, {post, Path, VSN}, Headers,
-		Body, State#state.callbackstate);
+	{NewState, Callback:handle_post(Path, VSN, Headers, Body, CBState)};
 call_callback(State, "OPTIONS", Path, VSN, Headers) ->
-	(State#state.callback):handle_options(State, {options, Path, VSN},
-		Headers, State#state.callbackstate);
+	Callback = State#state.callback,
+	CBState = State#state.callbackstate,
+	Callback:handle_options(Path, VSN, Headers, CBState);
 call_callback(State, "TRACE", Path, VSN, Headers) ->
-	(State#state.callback):handle_trace(State, {trace, Path, VSN}, Headers,
-		State#state.callbackstate).
+	Callback = State#state.callback,
+	CBState = State#state.callbackstate,
+	{State, Callback:handle_trace(Path, VSN, Headers, CBState)}.
 
 handle_upload(State, Headers) ->
 	case lists:keysearch("Content-Length", 1, Headers) of
